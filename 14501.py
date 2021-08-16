3ccc99d91b8223fb7883c45b600b929dcae97018
bitne <88517497+bitne@users.noreply.github.com>
2021-08-16 19:09:46 +0900

    Create 14501.py
    14501.py

---
+++ 14501.py
@@ -0,0 +1,19 @@
+n=int(input())
+t=[0]*(n+1)
+p=[0]*(n+1)
+for i in range(1,n+1):
+    t[i],p[i]=map(int,input().split())
+ans=0
+
+def go(day,sum):
+    global ans
+    if day==n+1:
+        ans=max(ans,sum)
+        return ##n+1이 되었으면 거기서 마치고 종료하기 위해서 return을 써줌. 안 그러면 index 
+    if day>n+1:
+        return
+    go(day+1,sum)
+    go(day+t[day],sum+p[day])
+
+go(1,0)
+print(ans)
